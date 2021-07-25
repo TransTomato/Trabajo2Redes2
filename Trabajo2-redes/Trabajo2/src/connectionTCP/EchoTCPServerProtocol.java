844967fe42654d811cdaa108364acc8f85736ae4
Michelle <mcorleone1946@gmail.com>
2021-07-25 18:16:40 -0500

    Correccion switch

--- Trabajo2-redes/Trabajo2/src/connectionTCP/EchoTCPServerProtocol.java
+++ Trabajo2-redes/Trabajo2/src/connectionTCP/EchoTCPServerProtocol.java
@@ -34,6 +34,7 @@ public class EchoTCPServerProtocol {
 			 switch(BankOptions.valueOf(message.split(",")[0])) {
 			 	case ABRIR_CUENTA:
 			 		try {
+			 			
 			 			invalidName(message.split(",")[1]);
 			 			bank.createAccount(message.split(",")[1]);
 						answer = "Cuenta creada correctamente. Usted es la cuenta # "+bank.accounts.size();
@@ -73,7 +74,7 @@ public class EchoTCPServerProtocol {
 				 	answer = "Su saldo en la cuenta #"+message.split(",")[1]+" es de "+balance;
 				break;
 			 	case CARGAR:
-				 	
+				 	ArrayList<String> transactions = ObjectReader.readTransactions(message.split(",")[1]);
 				 	answer = "Se ha cargado los datos del archivo: "+message.split(",")[1];
 				break;
 			 }
@@ -82,45 +83,6 @@ public class EchoTCPServerProtocol {
 			 System.out.println(oe.getMessage());
 			 answer=oe.getMessage();
 		 }
-		 
-		 switch(BankOptions.valueOf(message.split(",")[0])) {
-		 	case ABRIR_CUENTA:
-				bank.createAccount(message.split(",")[1]);
-				answer = "Cuenta creada correctamente. Usted es la cuenta # "+bank.accounts.size();
-		 	break;
-		 	case ABRIR_BOLSILLO:
-				bank.createPocket(message.split(",")[1]);
-				answer = "Bolsillo creado con éxito. Bolsillo # b"+message.split(",")[1];
-		 	break;
-		 	case CANCELAR_BOLSILLO:
-				bank.terminatePocket(message.split(",")[1]);
-				answer = "Bolsillo cancelado con éxito";
-		 	break;
-		 	case CANCELAR_CUENTA:
-		 		bank.terminateAccount(message.split(",")[1]);
-		 		answer = "Cuenta # "+message.split(",")[1]+" cancelada exitosamente";
-		 	break;
-		 	case DEPOSITAR:
-		 		bank.deposit(message.split(",")[1], Integer.parseInt(message.split(",")[2]));
-		 		answer = "Se depositó "+message.split(",")[2]+" en la cuenta # "+message.split(",")[1]+" correctamente";
-		 	break;
-		 	case RETIRAR:
-			 	bank.withdraw(message.split(",")[1], Integer.parseInt(message.split(",")[2]));
-			 	answer = "Se retiró "+message.split(",")[2]+" de la cuenta # "+message.split(",")[1]+" correctamente";
-			break;
-		 	case TRASLADAR:
-			 	bank.transfer(message.split(",")[1], Integer.parseInt(message.split(",")[2]));
-			 	answer = "Se trasladó "+message.split(",")[2]+" al bolsillo # "+message.split(",")[1]+" correctamente";
-			break;
-		 	case CONSULTAR:
-			 	int balance = bank.checkAccount(message.split(",")[1]);
-			 	answer = "Su saldo en la cuenta #"+message.split(",")[1]+" es de "+balance;
-			break;
-		 	case CARGAR:
-			 	ArrayList<String> transactions = ObjectReader.readTransactions(message.split(",")[1]);
-			 	answer = "Se ha cargado los datos del archivo: "+message.split(",")[1];
-			break;
-	 }
 		 
 		 
 		 toNetwork.println(answer);
